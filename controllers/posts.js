aa1f5f40a52ce19f22ff4b4e3ec053b69a054f67
Maxwell Calkin <maxwellcalkin@gmail.com>
2022-10-17 12:32:36 -0400

    deleted the fetch import

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,6 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment")
-import fetch from "node-fetch"
 
 module.exports = {
   getProfile: async (req, res) => {
