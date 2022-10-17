ee210847426b7b6de334aa22e94a454643234cf5
Maxwell Calkin <maxwellcalkin@gmail.com>
2022-10-17 12:49:00 -0400

    downgraded node-fetch to v2

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,7 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const Comment = require("../models/Comment")
-import fetch from "node-fetch"
+const fetch = require("node-fetch")
 
 module.exports = {
   getProfile: async (req, res) => {
